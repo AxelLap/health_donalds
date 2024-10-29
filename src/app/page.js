97972b8c5c37feaba7641c9291cda94129ed9d5a
Axel LAPLACE <axellaplace1709@gmail.com>
2024-10-29 10:12:09 +0100

    Toaster added to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import { Toaster } from "@/components/ui/sonner";
 import Header from "../components/ui/Header";
 
 export default function Home() {
   return (
     <main className=" flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <Toaster />
       <Header />
     </main>
   );
